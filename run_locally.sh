1dcc4ad664e8d0879a0baae7741f2786eb2b04b3
joXemMx <jonas.em@t-online.de>
2023-05-05 14:52:49 +0000

    update

--- run_locally.sh
+++ run_locally.sh
@@ -5,12 +5,13 @@ export MSNOVELIST_BASE=/home/vo87poq/MSNovelist-dev
 source /vol/software/anaconda3/etc/profile.d/conda.sh
 conda activate msnovelist-env
 cd /home/vo87poq/MSNovelist-dev
+mkdir -p trainings
 now=$(date +%s)
-mkdir $now
+mkdir trainings/$now
 
-for i in {0..9}
+for i in 1
 do
-    echo "training_id: '$i'" > $now/job_$i.yaml
-	echo "cv_fold: $i" >> $now/job_$i.yaml
-    python training_subclass.py -c config.yaml $now/job_$i.yaml
+    echo "training_id: '$i'" > trainings/${now}/job_${i}.yaml
+	echo "cv_fold: $i" >> trainings/${now}/job_${i}.yaml
+    python training_subclass.py -c config.yaml trainings/${now}/job_${i}.yaml |& tee trainings/${now}/job_${i}_output.txt
 done
