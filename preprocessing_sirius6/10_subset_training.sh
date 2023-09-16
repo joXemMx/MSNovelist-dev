2fc52089310ccd41539884031efaccf8d17e5d6e
joXemMx <jonas.em@t-online.de>
2023-09-16 10:23:35 +0000

    remove invalids from set, make folds equal size

--- preprocessing_sirius6/10_subset_training.sh
+++ preprocessing_sirius6/10_subset_training.sh
@@ -1,15 +1,85 @@
-
 #!/bin/bash
 
 DB_STEP1=$(sed -n 's/^ *db_step1: *//p' ~/msnovelist/target/log.yaml)
 DBNEW_UUID=$(python -c "import uuid; print(uuid.uuid4())")
-COMPOUNDS_LIMIT=1000000
+# full amount is 1.000.000
+COMPOUNDS_LIMIT=100000
 
+echo "creating target/sirius6-$DBNEW_UUID.db"
 
 sqlite3 ~/msnovelist/$DB_STEP1 << EOF
     ATTACH DATABASE '/home/vo87poq/msnovelist/target/sirius6-$DBNEW_UUID.db' AS target;
-    CREATE TABLE target.compounds AS
-        SELECT * FROM compounds ORDER BY perm_order LIMIT $COMPOUNDS_LIMIT;
+    CREATE TABLE target.compounds AS 
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold0'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold1'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold2'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold3'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold4'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold5'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold6'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold7'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold8'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold9'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    ORDER BY perm_order;
     CREATE INDEX IF NOT EXISTS idx_folds ON compounds (grp);
     CREATE INDEX IF NOT EXISTS idx_inchikeys ON compounds (inchikey1);
 EOF
--- preprocessing_sirius6/12_subset_crossval.sh
+++ preprocessing_sirius6/12_subset_crossval.sh
@@ -1,15 +1,85 @@
-
 #!/bin/bash
 
 DB_STEP2=~/msnovelist/target/sirius6-crossval-folds.db
 DBNEW_UUID=$(python -c "import uuid; print(uuid.uuid4())")
-COMPOUNDS_LIMIT=30000
+# full amount is 30.000
+COMPOUNDS_LIMIT=3000
 
+echo "creating target/sirius6-$DBNEW_UUID.db"
 
 sqlite3 $DB_STEP2 << EOF
     ATTACH DATABASE '/home/vo87poq/msnovelist/target/sirius6-$DBNEW_UUID.db' AS target;
-    CREATE TABLE target.compounds AS
-        SELECT * FROM compounds ORDER BY perm_order LIMIT $COMPOUNDS_LIMIT;
+    CREATE TABLE target.compounds AS 
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold0'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold1'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold2'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold3'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold4'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold5'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold6'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold7'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold8'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    UNION ALL
+    SELECT * FROM (
+        SELECT * FROM compounds
+        WHERE grp = 'fold9'
+        ORDER BY perm_order
+        LIMIT $COMPOUNDS_LIMIT
+    )
+    ORDER BY perm_order;
     CREATE INDEX IF NOT EXISTS idx_folds ON compounds (grp);
     CREATE INDEX IF NOT EXISTS idx_inchikeys ON compounds (inchikey1);
 EOF
